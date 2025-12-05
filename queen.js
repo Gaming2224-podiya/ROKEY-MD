ffb34b4e36fe803268a63452b0c67a190d9d4734
Gaming2224-podiya <accgit89@gmail.com>
2025-12-05 05:49:04 +0530

    Update owner number and bot name in queen.js

--- queen.js
+++ queen.js
@@ -20,7 +20,7 @@ const express = require("express");
 const app = express();
 const por = require("./session");
 const port = por.PORT;
-const ownerNumber = ["94717775628"];
+const ownerNumber = ["94740571366"];
 if (!app) return;
 // Session directory
 const SESSION_DIR = "./sessions";
@@ -29,7 +29,7 @@ const sess = require("./session");
 async function sessdl() {
   try {
     // Extract the Base64 encoded session data
-    const base64Data = sess.SESSION_ID.split("ANJU-XPRO~")[1];
+    const base64Data = sess.SESSION_ID.split("ROKEY_MD@")[1];
     if (!base64Data) {
       throw new Error("Invalid SESSION_ID format - missing Base64 data");
     }
@@ -91,7 +91,7 @@ async function connectToWA() {
   const getPrefix = () => config.PREFIX;
   const getWelcome = () => config.WELCOME;
   //===========================
-  console.log("🔥 QUEEN ANJU XPRO is starting...");
+  console.log("🔥 ROKEY_BOT is starting...");
   const { state, saveCreds } = await useMultiFileAuthState(
     __dirname + "/sessions/"
   );
@@ -119,9 +119,9 @@ async function connectToWA() {
       console.log("connected to whatsapp ✅");
       // Assuming `config` contains all the settings
       let up = `
-          🚀 **© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚 Connected Successfully!** ✅ 
+          @**ROKEY_MD Connected Successfully!**
           
-          --- **🎉 Welcome to © 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚!** 🎉 
+                     **Welcome to Rokey-bot!**  
           ✦» 𝚅𝚎𝚛𝚜𝚒𝚘𝚗 : ${require("./package.json").version}
           ✦» 𝙿𝚕𝚊𝚝𝚏𝚘𝚛𝚖 : ${os.platform()}
           ✦» 𝙷𝚘𝚜𝚝 : ${os.hostname()}
@@ -150,7 +150,7 @@ async function connectToWA() {
             config.AUTORECORDING ? "Enabled" : "Disabled"
           }
       
-          --- Thank you for using **© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚**. 
+          --- Thank you for using **ROKEY-MD**. 
           We're here to make your experience enjoyable and seamless. 
           If you need any help or have questions, don't hesitate to ask. 
           
@@ -159,22 +159,22 @@ async function connectToWA() {
       conn.sendMessage(conn.user.id, {
         text: up,
         contextInfo: {
-          mentionedJid: ["94717775628@s.whatsapp.net"], // specify mentioned JID(s) if any
+          mentionedJid: ["94740571366@s.whatsapp.net"], // specify mentioned JID(s) if any
           groupMentions: [],
           forwardingScore: 999,
           isForwarded: true,
           forwardedNewsletterMessageInfo: {
-            newsletterJid: "120363299978149557@newsletter",
-            newsletterName: "© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚",
+            newsletterJid: "@newsletter",
+            newsletterName: "ROKEY_MD",
             serverMessageId: 999,
           },
           externalAdReply: {
-            title: "© 𝚀𝚄𝙴𝙴𝙽 𝙰𝙽𝙹𝚄 𝗑ᴾᴿᴼ 💚",
-            body: " ©𝐌𝐑 𝐑𝐀𝐒𝐇𝐌𝐈𝐊𝐀 𝐎𝐅𝐂 💚",
+            title: "ROKEY_MD",
+            body: " MR.PEKSHA CDR",
             mediaType: 1,
-            sourceUrl: "https://github.com/Mrrashmika",
+            sourceUrl: "https://github.com/Gaming2224-podiya",
             thumbnailUrl:
-              "https://raw.githubusercontent.com/RASH-DATA/ANJU-DATA/refs/heads/main/LOGOS/6152181515400889311.jpg", // This should match the image URL provided above
+              "", // This should match the image URL provided above
             renderLargerThumbnail: false,
             showAdAttribution: true,
           },
